93a2c7239d9dacfe20b0305c4289ddc6a1eb7d76
minidonut <nycom13@gmail.com>
2022-01-02 00:13:17 +0900

    feat: update prompt format

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import * as tmp from 'tmp';
 import * as fs from 'fs-extra';
 import * as execa from 'execa';
 import * as path from 'path';
+import { URL } from 'url';
+
+type Tab = {
+  id: string;
+  url: string;
+  title: string;
+};
 
 const chromeCli = path.join(__dirname, '../bin/chrome-cli');
 
@@ -16,9 +23,21 @@ async function vimPrompt(text: string): Promise<string> {
   }
 }
 
+// [1234] ........ => '1234'
 function ids(tabs: string): string[] {
   return tabs.split('\n').reduce((acc: string[], n: string) => {
     const parsed = /^\[(\d+?)\]/.exec(n.trim());
+    if (parsed) {
+      acc.push(parsed[1]);
+    }
+    return acc;
+  }, []);
+}
+
+// ......... (1234) => '1234'
+function idsFromLast(tabs: string): string[] {
+  return tabs.split('\n').reduce((acc: string[], n: string) => {
+    const parsed = /\((\d+?)\)$/.exec(n.trim());
     if (parsed) {
       acc.push(parsed[1]);
     }
@@ -30,21 +49,56 @@ function printHelp() {
   console.log(`See https://github.com/minidonut/close-tab#Usage`);
 }
 
+async function findTab(tabId: string): Promise<Tab> {
+  const { stdout } = await execa(chromeCli, ['info', '-t', tabId]);
+  const [id, title, url] = stdout.split('\n').map(parse);
+  return {
+    id,
+    title,
+    url,
+  };
+
+  function parse(line: string) {
+    const parsed = /^.*?:\s(.*)$/.exec(line);
+    if (parsed == null) {
+      throw new Error(`Parse error: ${line}`);
+    }
+    return parsed[1].trim();
+  }
+}
+
+function printTabs(tabs: Tab[]): string {
+  return tabs.map(format).sort().join('\n');
+
+  function format(tab: Tab): string {
+    return `[${domainOf(tab.url)}] ${tab.title.slice(0, 140)} (${tab.id})`;
+  }
+
+  function domainOf(url: string) {
+    return new URL(url).host;
+  }
+}
+
 async function main(command?: string) {
   if (command == null) {
-    const { stdout: original } = await execa(chromeCli, ['list', 'tabs']);
+    const { stdout: rawList } = await execa(chromeCli, ['list', 'tabs']);
+    const currentIds = ids(rawList);
+    const currentTabs = await Promise.all(currentIds.map(findTab));
+    const original = printTabs(currentTabs);
+
     const filtered = await vimPrompt(original);
 
-    if (filtered === original) {
+    if (filtered.trim() === original.trim()) {
       console.log('aborted. 👋');
+      process.exit(0);
     }
 
-    const remainedIds = ids(filtered);
-    const toBeDeletedIds = ids(original).filter(id => !remainedIds.includes(id));
+    const remainedIds = idsFromLast(filtered);
+    const toBeDeletedTabs = currentTabs.filter(tab => !remainedIds.includes(tab.id));
 
-    for (const id of toBeDeletedIds) {
-      await execa(chromeCli, ['info', '-t', id], { stdio: 'inherit' });
-      await execa(chromeCli, ['close', '-t', id]);
+    for (const tab of toBeDeletedTabs) {
+      console.log(`${tab.title}\n${tab.url}\n`);
+      await execa(chromeCli, ['close', '-t', tab.id]);
     }
   } else if (['version', '--version', '-v'].includes(command)) {
     const pkgJson = await fs.readJson(path.join(__dirname, '..', 'package.json'));
